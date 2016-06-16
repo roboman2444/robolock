2928b3838ad1d901b4667a17732455818836b856
roboman2444 <roboman2444@gmail.com>
2016-06-16 10:50:27 -0400

    updated for archie

--- samplealert.sh
+++ samplealert.sh
@@ -2,12 +2,12 @@
 
 TIME=$(date)
 LOGDATE=$(date +"%Y-%m-%d_%H%M%S")
-LOGDIR=~/Dropbox/robolock-logs
+LOGDIR=/mnt/thing1/home/roboman2444/Dropbox/robolock-logs
 LOGFILE="$LOGDIR/alert-$LOGDATE.txt"
 IMAGEFILE="$LOGDIR/alert-$LOGDATE.jpeg"
 echo printing to log $LOGFILE
 date > $LOGFILE
-streamer -o $IMAGEFILE
+fswebcam $IMAGEFILE
 killall xmessage
 xmessage "ROBOLOCK: wrong password! last attempt: $LOGDATE" &
 #xmessage `whoami` &
